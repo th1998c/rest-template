0c230b3f078013e9a01a00250ad18e282aea868f
Thiago Carneiro da Silva <thiagocarneiro1998@gmail.com>
2022-11-20 20:19:37 -0300

    start project

---
+++ src/main/java/com/th/restTemplete/RestTempleteApplication.java
@@ -0,0 +1,13 @@
+package com.th.restTemplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTempleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTempleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thresttemplete/resttemplete/RestTempleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thresttemplete.resttemplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTempleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
